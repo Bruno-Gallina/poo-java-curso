c95fd16a4298ee6782763b76c1ead69d43e5b486
Bruno Gallina <53091700+Bruno-Gallina@users.noreply.github.com>
2022-09-19 14:26:54 -0300

    Create Main.java

---
+++ Aula05/Main.java
@@ -0,0 +1,29 @@
+package aula05;
+
+public class Aula05 { // Classe principal
+    
+    public static void main(String[] args) { // Método Main
+        // TODO code application logic here
+        ContaBanco p1 = new ContaBanco(); // Objeto p1
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("CC");
+        
+        
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+        p2.setDono("Creuza");
+        p2.abrirConta("CP");
+        
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(100);
+        
+        p1.fecharConta();
+        
+        p1.estadoAtual();
+        p2.estadoAtual();
+        
+    }
+    
+}
