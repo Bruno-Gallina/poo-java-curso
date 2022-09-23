dfbb01756bd9a377b039564768a6245a9c3524bd
Bruno Gallina <53091700+Bruno-Gallina@users.noreply.github.com>
2022-09-23 19:45:30 -0300

    Create Main.java

---
+++ Aula09/Main.java
@@ -0,0 +1,21 @@
+package aula09;
+
+public class Aula09 {
+
+    public static void main(String[] args) {
+        Pessoa [] p = new Pessoa[2];
+        Livro [] l = new Livro[3];
+        
+        p[0] = new Pessoa("Bruno", 18, "M");
+        p[1] = new Pessoa("Maria", 32, "F");
+        
+        l[0] = new Livro("Moonwalking with Einstein", "Dont Know", 300, p[0]);
+        l[1] = new Livro("Java for Dummies", "Prof Lino", 366, p[1]);
+        l[2] = new Livro("A arte da Guerra", "Samurai", 155, p[0]);
+        
+        l[0].abrir();
+        l[0].folhear(200);
+        System.out.println(l[0].detalhes());
+    }
+    
+}
