f123dfa34ef5b1b8cf4a8507b05f03de9a1addf3
Bruno Gallina <53091700+Bruno-Gallina@users.noreply.github.com>
2022-09-25 10:08:23 -0300

    Create Visitante.java

---
+++ Aula11/Visitante.java
@@ -0,0 +1,6 @@
+package aula11;
+
+public class Visitante extends Pessoa {
+    // Herança de Implementação / Herança"pobre";
+    // Sub-classe Visitante herda atributos e métodos da superclasse Pessoa
+}
